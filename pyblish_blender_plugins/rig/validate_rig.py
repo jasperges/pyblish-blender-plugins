abb8d315bd17fa95c4a81e85c597e816219f61fc
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2017-09-21 12:15:21 +0200

    All validations and actions work

--- pyblish_blender_plugins/rig/validate_rig.py
+++ pyblish_blender_plugins/rig/validate_rig.py
@@ -3,6 +3,8 @@
 # TODO:
 #     - check if several armatures are not in the same group?
 #     - check if an armature is parented
+#     - check if children or widgets have animation (add to NoAnimation)
+#     - check if all 'armature modifier' children are also parented to the armature
 
 import pyblish.api
 import bpy
@@ -73,6 +75,45 @@ class UngroupWidgets(pyblish.api.Action):
                         pass
 
 
+class RemoveAnimation(pyblish.api.Action):
+    """Remove all animation from objects"""
+
+    label = "Remove Animation"
+    on = "failed"
+    icon = "times"
+
+    def process(self, context, plugin):
+        for result in context.data['results']:
+            if result['error'] and plugin == result['plugin']:
+                instance = result['instance']
+                for member in instance:
+                    if member.animation_data:
+                        member.animation_data_clear()
+                        self.log.info("Removed animation data from %s" % member.name)
+
+
+class ResetTransforms(pyblish.api.Action):
+    """Reset all transforms"""
+
+    label = "Reset Rig Transforms"
+    on = "failed"
+    icon = "times"
+
+    def process(self, context, plugin):
+        for result in context.data['results']:
+            if result['error'] and plugin == result['plugin']:
+                instance = result['instance']
+                armature = instance.data('armature')
+                identity_matrix = Matrix.Identity(4)
+                if armature.matrix_world != identity_matrix:
+                    armature.matrix_world = identity_matrix
+                    self.log.info("Transformations of %s are reset" % armature.name)
+                for pose_bone in armature.pose.bones:
+                    if pose_bone.matrix_basis != identity_matrix:
+                        pose_bone.matrix_basis = identity_matrix
+                        self.log.info("Transformations of %s are reset" % pose_bone.name)
+
+
 class IsGrouped(pyblish.api.InstancePlugin):
     """Ensure the rig is in a group"""
 
@@ -138,15 +179,15 @@ class NoAnimation(pyblish.api.InstancePlugin):
 
     version = (0, 0, 1)
     order = pyblish.api.ValidatorOrder + 0.3
-    label = "Not Animated"
+    label = "No Animation"
     families = ["Rig"]
     optional = True
-    # TODO: add action to remove animation
+    actions = [RemoveAnimation]
 
     def process(self, instance):
-        armature = instance.data('armature')
-        if armature.animation_data and armature.animation_data.action:
-            raise ValueError("%s should not be animated" % armature.name)
+        for member in instance:
+            if member.animation_data and member.animation_data.action:
+                raise ValueError("%s should not be animated" % member.name)
 
 
 class NoTransforms(pyblish.api.InstancePlugin):
@@ -157,7 +198,7 @@ class NoTransforms(pyblish.api.InstancePlugin):
     label = "No Transforms"
     families = ["Rig"]
     optional = True
-    # TODO: add action to reset all transforms
+    actions = [ResetTransforms]
 
     def process(self, instance):
         armature = instance.data('armature')
