ae948db118361f7fca6038a80156bb057bf2afdf
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2017-09-27 12:35:14 +0200

    Cleanup of collect rig

--- pyblish_blender_plugins/rig/collect_rig.py
+++ pyblish_blender_plugins/rig/collect_rig.py
@@ -52,36 +52,24 @@ class CollectRig(pyblish.api.ContextPlugin):
 
     def process(self, context):
         # First check if the file is saved
-        # if not bpy.data.is_saved or bpy.data.is_dirty:
-            # raise Warning("Please save the file before publishing")
-        # try:
-        #     task_id = bpy.context.scene.piecekeeper.stalker.task_id
-        # except AttributeError:
-        #     self.log.error("Task ID could not be found in the scene")
-        #     raise
-        # if task_id < 0:
-        #     self.log.warning("The task ID is not set. Is this a valid production file?")
-        #     return
-        # db.setup()
-        # task = Task.query.filter_by(id=task_id).first()
-        # if not task:
-        #     self.log.warning("There is no task with ID %s" % task_id)
-        #     return
-        # if not task.name.lower() == 'rig':
-        #     self.log.info("This is not a rigging task, but %s" % task.name)
-        #     return
-
-        # for obj in bpy.data.objects:
-        #     if not obj.type == 'ARMATURE' or obj.name == 'metarig':
-        #         continue
-
-        #     for group in bpy.data.groups:
-        #         members = group.objects.values()
-        #         if obj in members:
-        #             widgets = [pb.custom_shape for pb in obj.pose.bones]
-        #             widgets.append(widgets[0].parent)
-        #             instance = context.create_instance(task.parent.name, family='Rig')
-        #             instance[:] = members + widgets
+        if not bpy.data.is_saved or bpy.data.is_dirty:
+            raise Warning("Please save the file before publishing")
+        try:
+            task_id = bpy.context.scene.piecekeeper.stalker.task_id
+        except AttributeError:
+            self.log.error("Task ID could not be found in the scene")
+            raise
+        if task_id < 0:
+            self.log.warning("The task ID is not set. Is this a valid production file?")
+            return
+        db.setup()
+        task = Task.query.filter_by(id=task_id).first()
+        if not task:
+            self.log.warning("There is no task with ID %s" % task_id)
+            return
+        if not task.name.lower() == 'rig':
+            self.log.info("This is not a rigging task, but %s" % task.name)
+            return
 
         for obj in bpy.data.objects:
             if not obj.type == 'ARMATURE' or obj.name == 'metarig':
